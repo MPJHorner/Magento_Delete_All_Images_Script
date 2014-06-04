ba6720e8b347dd099bc33d94c9472549c6e254de
MPJHorner <matt@matthorner.co.uk>
2014-06-04 10:22:48 +0100

    Added Retry Attempts & SOAP Error Messages Output
    Add Try/Catch to each call, retrying the amount of time specified by the
    user configuration.

--- DeleteAll.php
+++ DeleteAll.php
@@ -8,38 +8,95 @@
 $username = 'username'; //Magento SOAP Username
 $password = 'password'; //Magento SOAP Password
 $url = 'http://www.yourdomain.com/index.php/api/v2_soap/?wsdl'; //Magento SOAP URL
-/* Configuration */
+/* Credential Configuration */
 
 /* System Configuration */
+$retry_attempts = '3'; //Sets retry attempts when exceptions are caught.
 set_time_limit(0); //Ensure script doesn't timeout
-
+/* System Configuration */
 
 class MagentoConnection{
 	public $username;
 	public $password;
 	public $url;
 	
-	function __construct($username, $password, $url){
+	function __construct($username, $password, $url, $retry_attempts){
 		$this->username = $username;
 		$this->password = $password;
 		$this->url = $url;
+		$this->retry_attempts = $retry_attempts;
 	}
 	
 	function GetSession(){
-		$this->client = new SoapClient($this->url);
-		$this->session = $this->client->login($this->username,$this->password);
+		$GetSession_FaultCount = 0;
+		GetSession_TryAgain:
+		try{
+			$this->client = new SoapClient($this->url);
+		}catch (Exception $e){
+			$GetSession_FaultCount++;
+			if($this->retry_attempts < $GetSession_FaultCount){
+				goto GetSession_TryAgain;
+			}else{
+				echo '<br> Error using GetSession(new SoapClient) | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+			}
+		}
+		$GetSession_Login_FaultCount = 0;
+		GetSession_Login_TryAgain:
+		try{	
+			$this->session = $this->client->login($this->username,$this->password);
+		}catch (Exception $e){
+			$GetSession_Login_FaultCount++;
+			if($this->retry_attempts < $GetSession_Login_FaultCount){
+				goto GetSession_Login_TryAgain;
+			}else{
+				echo '<br> Error using GetSession(login) | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+			}
+		}
 	}
 	
 	function GetProductList(){
-		return $this->ProductList = $this->client->catalogProductList($this->session);
+		$GetProductList_FaultCount = 0;
+		GetProductList_TryAgain:
+		try{
+			$this->ProductList = $this->client->catalogProductList($this->session);
+		}catch (Exception $e){
+			$GetProductList_FaultCount++;
+			if($this->retry_attempts < $GetProductList_FaultCount){
+				goto GetProductList_TryAgain;
+			}else{
+				echo '<br> Error using GetProductList | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+			}
+		}
 	}
 	
 	function GetProductImageList($ProductID){
-		return $this->client->catalogProductAttributeMediaList($this->session, $ProductID);
+		$GetProductImageList_FaultCount = 0;
+		GetProductImageList_TryAgain:
+		try{
+			$this->client->catalogProductAttributeMediaList($this->session, $ProductID);
+		}catch (Exception $e){
+			$GetProductImageList_FaultCount++;
+			if($this->retry_attempts < $GetProductImageList_FaultCount){
+				goto GetProductImageList_TryAgain;
+			}else{
+				echo '<br> Error using GetProductImageList | ProductID:' . $ProductID . ' | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+			}
+		}
 	}
 	
 	function DeleteProductImage($ProductID, $Image){
-		return $this->client->catalogProductAttributeMediaRemove($this->session, $ProductID, $Image);
+		$DeleteProductImage_FaultCount = 0;
+		DeleteProductImage_TryAgain:
+		try{
+			$this->client->catalogProductAttributeMediaRemove($this->session, $ProductID, $Image);
+		}catch (Exception $e){
+			$DeleteProductImage_FaultCount++;
+			if($this->retry_attempts < $GetDeleteProductImage_FaultCount){
+				goto DeleteProductImage_TryAgain;
+			}else{
+				echo '<br> Error using DeleteProductImage | ProductID:' . $ProductID . ' | Image:' . $Image . ' | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+			}
+		}
 	}
 	
 	function DeleteAllImages(){
@@ -57,7 +114,7 @@ class MagentoConnection{
 }
 
 //Setup Magento Client
-$Connection = new MagentoConnection($username, $password, $url);
+$Connection = new MagentoConnection($username, $password, $url. $retry_attempts);
 $Connection->GetSession();
 $Connection->GetProductList();
 $Connection->DeleteAllImages();
