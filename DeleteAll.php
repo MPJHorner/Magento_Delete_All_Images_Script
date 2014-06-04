538dc7bb90f69c9815a2d877b3fd460a5b49f278
MPJHorner <matt@matthorner.co.uk>
2014-06-04 12:04:34 +0100

    Add Logging To File
    Added logging to txt file if enabled true in the settings. Script can
    take a VERY long time to execute as send request for each individual
    Product then Image. Viewing log file can see incremental progress whilst
    nothing is printed to the browser.

--- DeleteAll.php
+++ DeleteAll.php
@@ -15,6 +15,8 @@ $http_password = ''; //LEAVE BLANK UNLESS... You require HTTP authentication to 
 /* System Configuration */
 $retry_attempts = '3'; //Sets retry attempts when exceptions are caught.
 set_time_limit(0); //Ensure script doesn't timeout
+$log_to_file = false; //If enabled logging will be written to log file instead of printing and output.
+$log_file = 'Log.txt'; //If $log_to_file is configure true, this will be where the file is logged to.
 /* System Configuration */
 
 class MagentoConnection{
@@ -24,15 +26,30 @@ class MagentoConnection{
 	public $http_username;
 	public $http_password;
 	public $retry_attempts;
+	public $log_to_file;
+	public $log_file;
 	public $Deletions;
 	
-	function __construct($username, $password, $url, $retry_attempts $http_username, $http_password){
+	function __construct($username, $password, $url, $retry_attempts, $http_username, $http_password){
 		$this->username = $username;
 		$this->password = $password;
 		$this->url = $url;
 		$this->retry_attempts = $retry_attempts;
 		$this->http_username = $http_username;
 		$this->http_password = $http_password;
+	}
+	
+	function LogSettings($log_to_file, $log_file){
+		$this->log_to_file = $log_to_file;
+		$this->log_file = $log_file;
+	}
+	
+	function LogInfo($LogString){
+		if($this->log_to_file){
+			echo $LogString;
+			$LogString = $LogString.PHP_EOL;
+			file_put_contents($this->log_file, $LogString, FILE_APPEND | LOCK_EX);
+		}
 	}
 	
 	function GetSession(){
@@ -42,28 +59,40 @@ class MagentoConnection{
 			if($this->http_username != '' || $this->http_password != ''){
 				//Connect using HTTP Authentication
 				$this->client = new SoapClient($this->url,array('login' => $this->http_username, 'password' => $this->http_password));
+				$LogString = 'Connecting to URL(With HTTP Auth):' . $this->url;
+				$this->LogInfo($LogString);
 			}else{
 				//Connect without HTTP Authentication
 				$this->client = new SoapClient($this->url);
+				$LogString = 'Connecting to URL:' . $this->url. ' ...';
+				$this->LogInfo($LogString);
 			}
 		}catch (Exception $e){
 			$GetSession_FaultCount++;
 			if($this->retry_attempts < $GetSession_FaultCount){
+				$LogString = 'Attempt failed, attempt ' . $GetSession_FaultCount . '. Retrying ...';
+				$this->LogInfo($LogString);
 				goto GetSession_TryAgain;
 			}else{
-				echo '<br> Error using GetSession(new SoapClient) | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+				$LogString = 'Error using GetSession(new SoapClient) | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+				$this->LogInfo($LogString);
 			}
 		}
 		$GetSession_Login_FaultCount = 0;
 		GetSession_Login_TryAgain:
 		try{	
 			$this->session = $this->client->login($this->username,$this->password);
+			$LogString = 'Logging in and Creating session ...';
+			$this->LogInfo($LogString);
 		}catch (Exception $e){
 			$GetSession_Login_FaultCount++;
 			if($this->retry_attempts < $GetSession_Login_FaultCount){
+				$LogString = 'Attempt failed, attempt ' . $GetSession_Login_FaultCount . '. Retrying ...';
+				$this->LogInfo($LogString);
 				goto GetSession_Login_TryAgain;
 			}else{
-				echo '<br> Error using GetSession(login) | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+				$LogString = 'Error using GetSession(login) | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+				$this->LogInfo($LogString);
 			}
 		}
 	}
@@ -73,12 +102,17 @@ class MagentoConnection{
 		GetProductList_TryAgain:
 		try{
 			$this->ProductList = $this->client->catalogProductList($this->session);
+			$LogString = 'Retrieving Product List...';
+			$this->LogInfo($LogString);
 		}catch (Exception $e){
 			$GetProductList_FaultCount++;
 			if($this->retry_attempts < $GetProductList_FaultCount){
+				$LogString = 'Attempt failed, attempt ' . $GetProductList_FaultCount . '. Retrying ...';
+				$this->LogInfo($LogString);
 				goto GetProductList_TryAgain;
 			}else{
-				echo '<br> Error using GetProductList | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+				$LogString = 'Error using GetProductList | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+				$this->LogInfo($LogString);
 			}
 		}
 	}
@@ -88,12 +122,17 @@ class MagentoConnection{
 		GetProductImageList_TryAgain:
 		try{
 			$this->client->catalogProductAttributeMediaList($this->session, $ProductID);
+			$LogString = 'Retrieving ProductID:' . $ProductID . ' Image List...';
+			$this->LogInfo($LogString);
 		}catch (Exception $e){
 			$GetProductImageList_FaultCount++;
 			if($this->retry_attempts < $GetProductImageList_FaultCount){
+				$LogString = 'Attempt failed, attempt ' . $GetProductImageList_FaultCount . '. Retrying ...';
+				$this->LogInfo($LogString);
 				goto GetProductImageList_TryAgain;
 			}else{
-				echo '<br> Error using GetProductImageList | ProductID:' . $ProductID . ' | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+				$LogString = 'Error using GetProductImageList | ProductID:' . $ProductID . ' | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+				$this->LogInfo($LogString);
 			}
 		}
 	}
@@ -104,12 +143,17 @@ class MagentoConnection{
 		DeleteProductImage_TryAgain:
 		try{
 			$DeletionResult = $this->client->catalogProductAttributeMediaRemove($this->session, $ProductID, $Image);
+			$LogString = 'Deleting ProductID:' . $ProductID . ' Image:' . $Image . ' ...';
+			$this->LogInfo($LogString);
 		}catch (Exception $e){
 			$DeleteProductImage_FaultCount++;
 			if($this->retry_attempts < $GetDeleteProductImage_FaultCount){
+				$LogString = 'Attempt failed, attempt ' . $DeleteProductImage_FaultCount . '. Retrying ...';
+				$this->LogInfo($LogString);
 				goto DeleteProductImage_TryAgain;
 			}else{
-				echo '<br> Error using DeleteProductImage | ProductID:' . $ProductID . ' | Image:' . $Image . ' | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+				$LogString = 'Error using DeleteProductImage | ProductID:' . $ProductID . ' | Image:' . $Image . ' | Error Code:' . $e->faultcode . ' | Error Message:' . $e->getMessage();
+				$this->LogInfo($LogString);
 			}
 		}
 		return $DeletionResult;
@@ -133,6 +177,8 @@ class MagentoConnection{
 						}
 					}else{
 						$this->Deletions[] = array($Product->product_id, $Image->file);
+						$LogString = 'Deleted - ProductID:' . $Product->product_id . '. File:' . $Image->file;
+						$this->LogInfo($LogString);
 					}
 				endforeach;
 		endforeach;
@@ -140,7 +186,8 @@ class MagentoConnection{
 }
 
 //Setup Magento Client & Delete All Images
-$Connection = new MagentoConnection($username, $password, $url. $retry_attempts, $http_username, $http_password);
+$Connection = new MagentoConnection($username, $password, $url, $retry_attempts, $http_username, $http_password);
+$Connection->LogSettings($log_to_file, $log_file);
 $Connection->GetSession();
 $Connection->GetProductList();
 $Connection->DeleteAllImages();
